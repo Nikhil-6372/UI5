0683ac8c1a4d3ec0abce378905afe8126bc19e6f
Nikhil Moharana <vkdk210@gmail.com>
2025-09-30 16:01:05 +0000

    change something

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -2,11 +2,10 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ], (Controller) => {
     "use strict";
-
     return Controller.extend("com.demo.myhandson07.controller.View1", {
         onInit() {
         },
-        onPress: function(){
+        onPress: function () {
             this.getOwnerComponent().getRouter().navTo("RouteView2");
         }
     });
--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
     return Controller.extend("com.demo.myhandson07.controller.View2", {
         onInit() {
         },
-        onBackToView1: function(){
+        onBackToView1: function () {
             // this.getOwnerComponent().getRouter().navTo("RouteView1")
             history.go(-1)
         }
